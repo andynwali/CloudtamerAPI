a15a18ea1388ef0d2693d6b2822f8812c37ee054
Andy <joanderson4u@yahoo.com>
2020-04-20 01:48:25 -0400

    Create createdb.js
    Initial JS script to create AWS dynamoDB

---
+++ createdb.js
@@ -0,0 +1,42 @@
+const AWS = require('aws-sdk');
+// Create DynamoDB service object
+//AWS.config.loadFromPath('./config.json');
+const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+exports.handler = function(event, context, callback) {
+
+var tableParams = {
+  AttributeDefinitions: [
+    {
+      AttributeName: 'ID',
+      AttributeType: 'N'
+
+    }
+    
+  ],
+  KeySchema: [
+    {
+      AttributeName: 'ID',
+      KeyType: 'HASH'
+    }
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 5,
+    WriteCapacityUnits: 5
+  },
+  TableName: 'automobiles1',
+  StreamSpecification: {
+    StreamEnabled: false
+  }
+};
+
+ddb.createTable(tableParams, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data);
+
+  }
+});
+
+};
